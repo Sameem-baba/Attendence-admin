9c2e625a6e525aaed84d8ded687a051c576d7314
Sameem-baba <ssssameembaba@gmail.com>
2021-11-19 14:45:31 +0530

    teacher dash

--- components/TeacherDashboard.js
+++ components/TeacherDashboard.js
@@ -1,7 +1,47 @@
+import { collection, onSnapshot, query } from "firebase/firestore";
+import { useEffect, useState } from "react"
+import { auth, db } from "../firebase";
+import {
+    useAuthState
+} from "react-firebase-hooks/auth"
+
 function TeacherDashboard() {
+    const [present, setPresent] = useState([]);
+    const [user] = useAuthState(auth);
+
+    useEffect(
+        () =>
+            onSnapshot(
+                query(
+                    collection(db, 'attendence', 'user', 'present'),
+                ),
+                snapshot => setPresent(snapshot.docs)
+
+            ),
+        [db]
+    );
+
+    
     return (
         <div>
-            I am the Teacher
+            <h1 className="font-bold text-lg p-5">Admin Dashboard</h1>
+
+            <main>
+                <section className='flex pl-5 space-x-5 rounded-lg shadow-2xl'>
+                    <h3 className='font-semibold text-md '>Total Strength</h3>
+                    <p>38</p>
+                </section>
+
+                <section className='flex pl-5 space-x-5 rounded-lg shadow-2xl'>
+                    <h3 className='font-semibold text-md '>Present</h3>
+                    <p>{present.length}</p>
+                </section>
+
+                <section className='flex pl-5 space-x-5 rounded-lg'>
+                    <h3 className='font-semibold text-md '>Absent</h3>
+                    <p>{38 - present.length}</p>
+                </section>
+            </main>
         </div>
     )
 }
